b1c43d1eef9bf7660008805851393af859f15dc1
taco-tortilla <tortillataco0@gmail.com>
2024-12-31 02:09:43 +0900

    feat: mino rotation

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -9,6 +9,7 @@ public class Mino {
 
   public Block[] b = new Block[4];
   public Block[] tempB = new Block[4];
+  public int rotation = 0; // 4 directions (0 | 90 | 180 | 270)
   int autoDropCounter = 0;
 
   public void create(Color c) {
@@ -25,10 +26,53 @@ public class Mino {
   public void setXY(int x, int y) {
   }
 
-  public void updateXY(int direction) {
+  public void updateXY(int rotation) {
+    this.rotation = rotation;
+
+    b[0].x = tempB[0].x;
+    b[0].y = tempB[0].y;
+    b[1].x = tempB[1].x;
+    b[1].y = tempB[1].y;
+    b[2].x = tempB[2].x;
+    b[2].y = tempB[2].y;
+    b[3].x = tempB[3].x;
+    b[3].y = tempB[3].y;
+
   }
 
+  public void getRotation0() {
+  }
+
+  public void getRotation90() {
+  }
+
+  public void getRotation180() {
+  }
+
+  public void getRotation270() {
+  }
+
+
   public void update() {
+
+    if (KeyHandler.upPressed) {
+      switch (rotation) {
+        case 0:
+          getRotation90();
+          break;
+        case 90:
+          getRotation180();
+          break;
+        case 180:
+          getRotation270();
+          break;
+        case 270:
+          getRotation0();
+          break;
+      }
+
+      KeyHandler.upPressed = false;
+    }
 
     if (KeyHandler.downPressed) {
       b[0].y += Block.SIZE;
--- src/mino/MinoL.java
+++ src/mino/MinoL.java
@@ -19,7 +19,7 @@ public class MinoL extends Mino {
       Represented by an array
       b[1]
       b[0]
-      b[3] b[4]
+      b[2] b[3]
      */
 
     b[0].x = x;
@@ -30,6 +30,84 @@ public class MinoL extends Mino {
     b[2].y = b[0].y + Block.SIZE;
     b[3].x = b[0].x + Block.SIZE;
     b[3].y = b[0].y + Block.SIZE;
+  }
+
+  @Override
+  public void getRotation0() {
+    /*
+      □
+      □
+      □ □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x;
+    tempB[1].y = b[0].y - Block.SIZE;
+    tempB[2].x = b[0].x;
+    tempB[2].y = b[0].y + Block.SIZE;
+    tempB[3].x = b[0].x + Block.SIZE;
+    tempB[3].y = b[0].y + Block.SIZE;
+
+    updateXY(0);
+  }
+
+  @Override
+  public void getRotation90() {
+    /*
+      □ □ □
+      □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x + Block.SIZE;
+    tempB[1].y = b[0].y;
+    tempB[2].x = b[0].x - Block.SIZE;
+    tempB[2].y = b[0].y;
+    tempB[3].x = b[0].x - Block.SIZE;
+    tempB[3].y = b[0].y + Block.SIZE;
+
+    updateXY(90);
+  }
+
+  @Override
+  public void getRotation180() {
+    /*
+      □ □
+        □
+        □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x;
+    tempB[1].y = b[0].y + Block.SIZE;
+    tempB[2].x = b[0].x;
+    tempB[2].y = b[0].y - Block.SIZE;
+    tempB[3].x = b[0].x - Block.SIZE;
+    tempB[3].y = b[0].y - Block.SIZE;
+
+    updateXY(180);
+  }
+
+  @Override
+  public void getRotation270() {
+    /*
+          □
+      □ □ □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x - Block.SIZE;
+    tempB[1].y = b[0].y;
+    tempB[2].x = b[0].x + Block.SIZE;
+    tempB[2].y = b[0].y;
+    tempB[3].x = b[0].x + Block.SIZE;
+    tempB[3].y = b[0].y - Block.SIZE;
+
+    updateXY(270);
   }
 
 }
