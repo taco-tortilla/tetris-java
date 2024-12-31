e7707bdb9008008d45555fcbec91213c73aea879
taco-tortilla <tortillataco0@gmail.com>
2024-12-31 12:08:25 +0900

    feat: Z mino

--- src/mino/MinoZ.java
+++ src/mino/MinoZ.java
@@ -1,5 +1,113 @@
 package mino;
 
-public class MinoZ {
+import java.awt.Color;
 
+public class MinoZ extends Mino {
+
+  public MinoZ() {
+    super.create(Color.red);
+  }
+
+  @Override
+  public void setXY(int x, int y) {
+    /*
+      Z mino
+      □ □
+        □ □
+
+      Represented by an array
+       b[1] b[0]
+            b[3] b[2]
+     */
+
+    b[0].x = x;
+    b[0].y = y;
+    b[1].x = b[0].x - Block.SIZE;
+    b[1].y = b[0].y;
+    b[2].x = b[0].x + Block.SIZE;
+    b[2].y = b[0].y + Block.SIZE;
+    b[3].x = b[0].x;
+    b[3].y = b[0].y + Block.SIZE;
+
+  }
+
+
+  @Override
+  public void getRotation0() {
+    /*
+        □
+      □ □
+      □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x;
+    tempB[1].y = b[0].y - Block.SIZE;
+    tempB[2].x = b[0].x - Block.SIZE;
+    tempB[2].y = b[0].y + Block.SIZE;
+    tempB[3].x = b[0].x - Block.SIZE;
+    tempB[3].y = b[0].y;
+
+    updateXY(0);
+  }
+
+  @Override
+  public void getRotation90() {
+    /*
+      □ □
+        □ □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x + Block.SIZE;
+    tempB[1].y = b[0].y;
+    tempB[2].x = b[0].x - Block.SIZE;
+    tempB[2].y = b[0].y - Block.SIZE;
+    tempB[3].x = b[0].x;
+    tempB[3].y = b[0].y - Block.SIZE;
+
+    updateXY(90);
+  }
+
+  @Override
+  public void getRotation180() {
+    /*
+        □
+      □ □
+      □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x;
+    tempB[1].y = b[0].y + Block.SIZE;
+    tempB[2].x = b[0].x + Block.SIZE;
+    tempB[2].y = b[0].y - Block.SIZE;
+    tempB[3].x = b[0].x + Block.SIZE;
+    tempB[3].y = b[0].y;
+
+    updateXY(180);
+  }
+
+  @Override
+  public void getRotation270() {
+    /*
+      □ □
+        □ □
+     */
+
+    tempB[0].x = b[0].x;
+    tempB[0].y = b[0].y;
+    tempB[1].x = b[0].x - Block.SIZE;
+    tempB[1].y = b[0].y;
+    tempB[2].x = b[0].x + Block.SIZE;
+    tempB[2].y = b[0].y + Block.SIZE;
+    tempB[3].x = b[0].x;
+    tempB[3].y = b[0].y + Block.SIZE;
+
+    updateXY(270);
+
+  }
 }
