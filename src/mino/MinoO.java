248113806fd63a1aec715ae8348cf13c9d9240b4
taco-tortilla <tortillataco0@gmail.com>
2024-12-31 11:33:07 +0900

    feat: O mino

--- src/mino/MinoO.java
+++ src/mino/MinoO.java
@@ -1,5 +1,34 @@
 package mino;
 
-public class MinoO {
+import java.awt.Color;
+
+public class MinoO extends Mino {
+
+  public MinoO() {
+    create(Color.yellow);
+  }
+
+  @Override
+  public void setXY(int x, int y) {
+    /*
+      O mino
+      □ □
+      □ □
+
+      Represented by an array
+      b[0] b[1]
+      b[2] b[3]
+     */
+
+    b[0].x = x;
+    b[0].y = y;
+    b[1].x = b[0].x + Block.SIZE;
+    b[1].y = b[0].y;
+    b[2].x = b[0].x;
+    b[2].y = b[0].y + Block.SIZE;
+    b[3].x = b[0].x + Block.SIZE;
+    b[3].y = b[0].y + Block.SIZE;
+
+  }
 
 }
